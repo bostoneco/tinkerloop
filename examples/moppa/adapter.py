b38e9a07ff255532524ba30865134e0d65f68925
bostone <bostone@gmail.com>
2026-03-09 23:17:03 -0700

    Add guided cleanup interview scenarios

--- examples/moppa/adapter.py
+++ examples/moppa/adapter.py
@@ -38,6 +38,7 @@ class MoppaAdapter(CommandAppAdapter):
     def preflight(self, *, user_id: str) -> PreflightResult:
         state_files = [
             ".tinkerloop_cleanup_state.json",
+            ".tinkerloop_guided_cleanup_state.json",
             ".tinkerloop_reclassification_state.json",
             ".tinkerloop_routing_state.json",
             ".tinkerloop_compose_state.json",
